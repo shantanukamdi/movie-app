4249a950c20f71bda7c08cedf042af4b6a8762f3
Shantanu Kamdi <kamdishantanu@gmail.com>
2019-03-30 16:12:52 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 class App extends Component {
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
@@ -11,14 +12,6 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
